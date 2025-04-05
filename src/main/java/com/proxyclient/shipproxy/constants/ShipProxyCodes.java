1ada574be6dbcfe91d79864681a19161276d5c58
Harikrishnan <harisgkrishnan@yahoo.com>
2025-04-05 11:47:17 +0530

    host changes

--- src/main/java/com/proxyclient/shipproxy/constants/ShipProxyCodes.java
+++ src/main/java/com/proxyclient/shipproxy/constants/ShipProxyCodes.java
@@ -2,7 +2,7 @@ package com.proxyclient.shipproxy.constants;
 
 public class ShipProxyCodes {
 
-    public static final String OFFSHORE_HOST = "localhost";
+    public static final String OFFSHORE_HOST = "offshoreserver";
     public static final int OFFSHORE_PORT = 9000;
     public static final int MAX_RETRIES = 10;
     public static final long RETRY_DELAY_MS = 2000;
